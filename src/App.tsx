711e385d9228c4157a0debbf0808447f23dc876f
fakebmarchete <leonardo.ngra@gmail.com>
2022-08-24 12:23:07 -0300

    global

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,13 @@
+import { GlobalStyle } from "./styles/global";
 
 
 export function App() {
   return (
     <div className="App">
      <h1>Hello Word</h1>
+     <GlobalStyle />
+
+   
     </div>
   );
 }
---
+++ src/styles/global.ts
@@ -0,0 +1,60 @@
+import { createGlobalStyle } from "styled-components";
+
+export const GlobalStyle = createGlobalStyle`
+
+    :root{
+        --background: #F0F2F5;
+        --red: #F52F4F;
+        --blue: #5429CC;
+
+        --blue-light: #6933FF;
+
+        --text-title: #363F5F;
+        --text-body: #969CB3;
+
+        --snape: #FFFFFF;
+    }
+
+    *{
+        margin: 0;
+        padding: 0;
+        box-sizing: border-box;
+    }
+
+    // font-size:16px(desktop)
+    // 1 rem = 16px(tamanho da nossa tela)
+    html {
+        @media (max-width: 1080px){
+            font-size: 93.75%; //15px
+        }
+
+        @media (max-width: 720px) {
+            font-size: 87.5%; // 14px
+        }
+    }
+
+    body{
+        background: var(--background);
+        // visibilidade nos navegadores
+        -webkit-font-smoothing: antialiased
+    }
+
+    body, input, textarea, button {
+        font-family: 'Poppins', sans-serif;
+        font-weight: 400;
+    }
+
+    h1, h2, h3, h4, h5, h6, strong {
+        font-weight: 600;
+    }
+
+    button {
+        cursor: pointer;
+    }
+
+    [disable] {
+        opacity: 0.6;
+        cursor: not-allowed;
+    }
+
+`;
