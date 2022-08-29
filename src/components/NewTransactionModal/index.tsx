249daf387368f348fa67285617fbd7fac7c6a344
fakebmarchete <leonardo.ngra@gmail.com>
2022-08-29 16:17:47 -0300

    estilizando modal

--- src/components/NewTransactionModal/index.tsx
+++ src/components/NewTransactionModal/index.tsx
@@ -1,18 +1,51 @@
 import Modal from 'react-modal';
+import { Container } from './styles';
+import closeImg from '../../assets/close.svg';
 
 
 interface newTransactionModalProps {
     isOpen: boolean;
     onRequestClose: () => void;
 }
+
 export function NewTransactionModal({isOpen, onRequestClose}: newTransactionModalProps){
 
     return (
         <Modal 
           isOpen={isOpen}
           onRequestClose={onRequestClose}
+          overlayClassName='react-modal-overlay' // mudar estilização
+          className='react-modal-content' // mudar estilização
          >
-            <h2>Cadastrar Transação</h2>
+
+        <button 
+            type='button' 
+            onClick={onRequestClose}
+            className='react-modal-close'
+            >
+            <img src={closeImg} alt="Fechar Modal" />
+        </button>    
+        <Container>
+                 <h2>Cadastrar Transação</h2>
+
+                 <input 
+                    placeholder='Título' 
+                 />
+
+                <input
+                    type='number' 
+                    placeholder='Valor' 
+                 />
+
+                <input 
+                    placeholder='Categoria' 
+                 />
+
+                <button type="submit">
+                    Cadastrar
+                </button> 
+        </Container>
+
         </Modal>
     );
 };
