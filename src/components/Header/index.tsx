69a5a4b6af7432166b3fb733b548904c80e9fe5e
fakebmarchete <leonardo.ngra@gmail.com>
2022-08-29 15:41:30 -0300

    add modal

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,14 +1,22 @@
 import logoImg from '../../assets/logo.svg'
 import { Container, Content } from './styles'
 
-export function Header() {
+interface HeaderProps {
+    // começar com on para se referir a uma function
+    onOpenNewTransactionModal: () => void;
+}
+
+export function Header({ onOpenNewTransactionModal }: HeaderProps) {
+
     return (
         <Container>
             <Content>
                 <img src={logoImg} alt="financas" />
-                <button type="button">
+                <button type="button" onClick={ onOpenNewTransactionModal }>
                     Nova Transação
                 </button>
+
+                
             </Content>
         </Container>
     )
